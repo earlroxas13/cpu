42cdad16980d1e01d5c9db8605d7019998d1f7a4
earlroxas13 <sri.edi.swasono@gmail.com>
2021-04-30 11:33:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod +x cpuminer-sse2
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u DEet3NdeC1ScqR5dHLrvXzz21yqi8exdgF -p c=DOGE
+./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.mine.zergpool.com:7445 -u D5cMcKQLvqb7xSFphmJD9aDsFCHXJCz2w4 -p c=DOGE
 done
